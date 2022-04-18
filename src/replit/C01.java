b69442ff0cbf4a4cd92a37c34e0db69c61f0fa44
Tuba Tiryaki <tubatiryaki25@gmail.com>
2022-04-19 01:38:16 +0300

    Replit soru cozumu

--- src/replit/C01.java
+++ src/replit/C01.java
@@ -1,2 +1,20 @@
-package replit;public class C01 {
+package replit;
+
+public class C01 {
+    public static void main(String[] args) {
+        //1'den 100'e kadar olan doğal sayıların toplamını bulan programı yazınız.
+        //OutPut:
+        //Sayilarin Toplami : 5050
+
+        int toplam=0;
+        for (int i = 0; i <=100 ; i++) {
+            for (int j =i; j <100 ; j++) {
+                toplam++;
+
+            }
+        }
+        System.out.println("toplam = " + (toplam));
+
+
+    }
 }
--- src/replit/C02.java
+++ src/replit/C02.java
@@ -1,2 +1,25 @@
-package replit;public class C02 {
+package replit;
+
+import java.util.Scanner;
+
+public class C02 {
+    public static void main(String[] args) {
+        //Kullanıcının girdiği sayıyı tersine çeviren bir java programı yazınız. (Mulakat Sorusu)
+        //Input :1238
+        //Output :Girilen Numananin Tersi: 8321
+
+        Scanner scan=new Scanner(System.in);
+        System.out.print("bir 4 basamakli bir sayi giriniz: ");
+        int sayi= scan.nextInt();
+        while (sayi!=0){
+            System.out.print(sayi%10);
+            sayi/=10;
+        }
+
+
+
+
+
+        
+    }
 }
